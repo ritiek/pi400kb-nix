4d50eeeb8653fcf0bf4185cd2ede2f44fe526163
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-11-17 18:10:53 +0530

    Systemd now correctly picks up resulting binary

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Raw HID keyboard forwarder to turn the Pi 400 into a USB keyboard ";
+  description = "Raw HID keyboard forwarder to turn the Pi 400 into a USB keyboard";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
@@ -12,8 +12,7 @@
   outputs = { self, nixpkgs, pi400kb }:
     let
       pkgs = import nixpkgs { system = "aarch64-linux"; };
-    in {
-      packages.aarch64-linux.default = pkgs.stdenv.mkDerivation {
+      pi400kbPkg = pkgs.stdenv.mkDerivation {
         pname = "pi400kb";
         version = "0.0.1";
 
@@ -29,27 +28,48 @@
 
         cmakeFlags = [
           "-D HOOK_PATH=./hook.sh"
-          # FIXME: Find a workaround to refer to $out and have it evaluate
-          # correctly. Currently, $out gets treated as a literal here.
-          # "-D HOOK_PATH=$out/hook.sh"
         ];
 
         installPhase = ''
-          install -Dm755 pi400kb $out/pi400kb
-          install -Dm755 ../hook.sh $out/hook.sh
+          install -Dm755 pi400kb $out/bin/pi400kb
+          install -Dm755 ../hook.sh $out/bin/hook.sh
         '';
 
-        # Replace "led0" by "PWR" to comply with upstream changes.
         postFixup = ''
-          substituteInPlace $out/hook.sh --replace led0 PWR
+          substituteInPlace $out/bin/hook.sh --replace led0 PWR
         '';
+      };
+    in {
+      packages.aarch64-linux.default = pi400kbPkg;
+
+      nixosModules = rec {
+        pi400kb = { config, lib, options, ... }: {
+          options.services.pi400kb.enable = lib.mkOption {
+            type = lib.types.bool;
+            default = false;
+            description = "Enable the Raw HID keyboard forwarder to turn the Pi 400 into a USB keyboard service.";
+          };
+
+          config.systemd.services.pi400kb = {
+            enable = config.services.pi400kb.enable;
+            description = "pi400kb USB OTG Keyboard & Mouse forwarding";
+            serviceConfig = {
+              ExecStart = "${pi400kbPkg.out}/bin/pi400kb";
+              User = "root";
+              Group = "root";
+              Type = "simple";
+              Restart = "on-failure";
+            };
+            wantedBy = [ "multi-user.target" ];
+          };
+        };
       };
 
       nixosConfigurations = {
         mySystem = nixpkgs.lib.nixosSystem {
           system = "aarch64-linux";
           modules = [
-            ./pi400kb-service.nix 
+            self.nixosModules.pi400kb
           ];
         };
       };

132735c2ec00a5390e1fdd2c05e4daeee9f4be56
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-11-18 00:12:19 +0530

    Minimalize flake.nix a bit

--- flake.nix
+++ flake.nix
@@ -9,69 +9,56 @@
     };
   };
 
-  outputs = { self, nixpkgs, pi400kb }:
-    let
-      pkgs = import nixpkgs { system = "aarch64-linux"; };
-      pi400kbPkg = pkgs.stdenv.mkDerivation {
-        pname = "pi400kb";
-        version = "0.0.1";
+  outputs = { self, nixpkgs, pi400kb }: {
+    system = "aarch64-linux";
+    pkgs = import nixpkgs { system = self.system; };
 
-        src = pi400kb;
+    packages.${self.system}.default = self.pkgs.stdenv.mkDerivation {
+      pname = "pi400kb";
+      version = "0.0.1";
+      src = pi400kb;
+      buildInputs = [ self.pkgs.cmake ];
+      nativeBuildInputs = [ self.pkgs.libconfig ];
+      cmakeFlags = [ "-D HOOK_PATH=./hook.sh" ];
+      installPhase = ''
+        install -Dm755 pi400kb $out/bin/pi400kb
+        install -Dm755 ../hook.sh $out/bin/hook.sh
+      '';
+      postFixup = ''
+        substituteInPlace $out/bin/hook.sh --replace led0 PWR
+      '';
+    };
 
-        buildInputs = with pkgs; [
-          cmake
-        ];
-
-        nativeBuildInputs = with pkgs; [
-          libconfig
-        ];
-
-        cmakeFlags = [
-          "-D HOOK_PATH=./hook.sh"
-        ];
-
-        installPhase = ''
-          install -Dm755 pi400kb $out/bin/pi400kb
-          install -Dm755 ../hook.sh $out/bin/hook.sh
-        '';
-
-        postFixup = ''
-          substituteInPlace $out/bin/hook.sh --replace led0 PWR
-        '';
-      };
-    in {
-      packages.aarch64-linux.default = pi400kbPkg;
-
-      nixosModules = rec {
-        pi400kb = { config, lib, options, ... }: {
-          options.services.pi400kb.enable = lib.mkOption {
-            type = lib.types.bool;
-            default = false;
-            description = "Enable the Raw HID keyboard forwarder to turn the Pi 400 into a USB keyboard service.";
-          };
-
-          config.systemd.services.pi400kb = {
-            enable = config.services.pi400kb.enable;
-            description = "pi400kb USB OTG Keyboard & Mouse forwarding";
-            serviceConfig = {
-              ExecStart = "${pi400kbPkg.out}/bin/pi400kb";
-              User = "root";
-              Group = "root";
-              Type = "simple";
-              Restart = "on-failure";
-            };
-            wantedBy = [ "multi-user.target" ];
-          };
+    nixosModules = {
+      pi400kb = { config, lib, options, ... }: {
+        options.services.pi400kb.enable = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = "Enable the Raw HID keyboard forwarder to turn the Pi 400 into a USB keyboard service.";
         };
-      };
 
-      nixosConfigurations = {
-        mySystem = nixpkgs.lib.nixosSystem {
-          system = "aarch64-linux";
-          modules = [
-            self.nixosModules.pi400kb
-          ];
+        config.systemd.services.pi400kb = {
+          enable = config.services.pi400kb.enable;
+          description = "pi400kb USB OTG Keyboard & Mouse forwarding";
+          serviceConfig = {
+            ExecStart = "${self.packages.${self.system}.default.out}/bin/pi400kb";
+            User = "root";
+            Group = "root";
+            Type = "simple";
+            Restart = "on-failure";
+          };
+          wantedBy = [ "multi-user.target" ];
         };
       };
     };
+
+    # nixosConfigurations = {
+    #   mySystem = nixpkgs.lib.nixosSystem {
+    #     system = self.system;
+    #     modules = [
+    #       self.nixosModules.pi400kb
+    #     ];
+    #   };
+    # };
+  };
 }
